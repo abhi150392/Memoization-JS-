cceb3062f0f2592d36a213005a8b1ce0cff1d867
Abhishek Vijayakumar <abhishekwhizzer@gmail.com>
2023-07-25 15:09:41 -0500

    modified memo function for multiple args

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-function add(n) {
-  return 10 + n;
+function add(...data) {
+  return data.reduce((prev, curr) => {
+    return curr + prev;
+  }, 0);
 }
 
 const memo = (fn) => {
   let cache = {};
-  return (x) => {
-    if (cache[x]) {
+  return (...args) => {
+    const keys = JSON.stringify(args);
+    if (cache[keys]) {
       console.log("return result from cache");
-      return cache[x];
+      return cache[keys];
     } else {
       console.log("calculate the result");
-      const result = fn(x);
-      cache[x] = result;
+      const result = fn(...args);
+      cache[keys] = result;
       return result;
     }
   };
 };
 
 const calculate = memo(add);
-console.log(calculate(10));
-console.log(calculate(10));
-console.log(calculate(20));
-console.log(calculate(20));
+console.log(calculate(10, 20, 30));
+console.log(calculate(10, 20, 30));
+console.log(calculate(10, 20, 50));
+console.log(calculate(10, 20, 50));
 /* console.log(add(5));
 console.log(add(5)); */
